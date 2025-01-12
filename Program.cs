4ad032c398c6169e899a57943daa0901fea85240
pkrzysiek <pkrzysiek24@gmail.com>
2025-01-12 13:44:18 +0100

    Add choice basic menu selection

--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? playerName;
+
+            string? choice="";
+            do
+            {
+                Console.WriteLine("Welcome to the Math Game! Please tell me your username: ");
+                playerName = Console.ReadLine();
+                var startDate=DateTime.Now;
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"Hi {playerName}, It's {startDate}");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1.Addition Game");
+                Console.WriteLine("2.Subtraction Game");
+                Console.WriteLine("3.Multiplication Game");
+                Console.WriteLine("4.Divison Game");
+                Console.WriteLine("5.Player stats");
+                Console.WriteLine("Your choice: ");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+
+
+                }
+            } while (choice.ToLower() != "exit");
+            
+
+            
         }
     }
 }
